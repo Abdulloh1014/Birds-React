599f5176ef497a87a2bb407a947f30a3e9e88552
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2026-04-21 09:36:24 +0500

    fix: deploy.sh fileda togirlash

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
+# #!/bin/bash
+
+# # PRODUCTION 
+# git reset --hard
+# git checkout master
+# git pull origin master
+
+# npm i yarn -g
+# yarn global add serve
+# yarn
+# yarn run build
+# pm2 start "yarn run start:prod" --name=BIRDS-REACT
+
+
 #!/bin/bash
 
 # PRODUCTION 
@@ -5,8 +19,14 @@ git reset --hard
 git checkout master
 git pull origin master
 
+# Kerakli paketlarni o'rnatish
 npm i yarn -g
 yarn global add serve
-yarn
+yarn install
+
+# Build qilish
 yarn run build
+
+# PM2: Agar bor bo'lsa restart qiladi, bo'lmasa yangi ochadi
+pm2 delete BIRDS-REACT 2>/dev/null || true
 pm2 start "yarn run start:prod" --name=BIRDS-REACT
